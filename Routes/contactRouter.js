d3abe36a177c776991d2956cb050584dd7471939
Brijendra <bp07072002@gmail.com>
2022-10-06 12:33:20 +0530

    Change the status Completed or Notcompleted

--- Routes/contactRouter.js
+++ Routes/contactRouter.js
@@ -7,5 +7,6 @@ router.get("/api/get", ContactController.ShowAllContact);
 router.get("/api/get/:id", ContactController.ShowSingleContact);
 router.put("/api/update/:id", ContactController.UpdateContact);
 router.delete("/api/remove/:id", ContactController.DeleteContact);
+router.put("/api/update/status/:id", ContactController.ChangeStatusContact);
 
 export default router;
